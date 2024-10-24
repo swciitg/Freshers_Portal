d3706d5284b7127541dfffb3e45ceccfcf5eb21f
Avinash Gupta <guptaavinash302@gmail.com>
2024-10-24 17:09:15 +0530

    made hostel list pages

---
+++ frontend/src/pages/ListHostelPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../components/HostelPage/Main'
+
+const ListHostelPage = () => {
+  return (
+    <div>
+      <Main/>
+    </div>
+  )
+}
+
+export default ListHostelPage
