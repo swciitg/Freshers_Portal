5244fd11e224ecb33d8e22281c267bb0ba935938
Avinash Gupta <guptaavinash302@gmail.com>
2024-10-27 17:57:13 +0530

    Added FacilitiesPage

---
+++ frontend/src/pages/FacilitiesPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../components/FacilitiesPage/Main'
+
+const FacilitiesPage = () => {
+  return (
+    <div>
+        <Main/>
+    </div>
+  )
+}
+
+export default FacilitiesPage
