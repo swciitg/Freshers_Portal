d366256b4bf14a7f8e2b8ed37fc884b67875dbdd
Avinash Gupta <guptaavinash302@gmail.com>
2024-10-27 17:46:55 +0530

    added tech and sports board

---
+++ frontend/src/pages/SportsBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../components/SportsBoard/Main'
+
+const SportsBoard = () => {
+  return (
+    <div>
+        <Main/>
+    </div>
+  )
+}
+
+export default SportsBoard
---
+++ frontend/src/pages/TechBoard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Main from '../components/TechBoard/Main'
+
+const TechBoard = () => {
+  return (
+    <div>
+        <Main/>
+    </div>
+  )
+}
+
+export default TechBoard
